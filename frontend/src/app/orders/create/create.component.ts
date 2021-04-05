d68f9c1d2eb05a256d912d4b9db00adf4fb64e58
danakhreis <dana_dalaa_2000@hotmail.de>
2021-04-05 17:01:52 +0200

    update create component ts

--- frontend/src/app/orders/create/create.component.ts
+++ frontend/src/app/orders/create/create.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+  form: FormGroup;
+  orders: Orders;
 
-  constructor() { }
+
+  constructor(
+  private cs: BackendService,
+      private fb: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router
+  ) }
+  this.form = this.fb.group(
+        {
+          firstNameControl: ['', Validators.required],
+          lastNameControl: ['', Validators.required],
+          emailControl: ['', Validators.required],
+          orderNrControl: ['', Validators.required]
+        }
+    );
+    this.orders = { id: 0, firstname: '', lastname: '', email: '', orderNr: 0,};
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    console.warn(this.form.value);
+    const values = this.form.value;
+    this.orders.firstname = values.firstNameControl;
+    this.orders.lastname = values.lastNameControl;
+    this.orders.email = values.emailControl;
+    this.orders.orderNr = values.orderNrControl;
+    console.log(this.orders);
+    this.cs.create(this.orders);
+    this.router.navigate(['/read']);
+  }
+
+  cancel(): void {
+    this.router.navigate(['/read']);
+  }
 }
+
